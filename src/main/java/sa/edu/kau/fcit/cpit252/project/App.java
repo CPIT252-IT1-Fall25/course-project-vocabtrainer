c3f1102e13f46f66f4cadaded7744c4c1ab59a68
vAbdullh <136025903+vAbdullh@users.noreply.github.com>
2025-11-09 15:24:46 +0300

    init core classes, closes #1

--- src/main/java/sa/edu/kau/fcit/cpit252/project/App.java
+++ src/main/java/sa/edu/kau/fcit/cpit252/project/App.java
@@ -5,6 +5,6 @@ package sa.edu.kau.fcit.cpit252.project;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to Vocab Trainer!");
     }
 }
---
+++ src/main/java/sa/edu/kau/fcit/cpit252/project/Word.java
@@ -0,0 +1,6 @@
+package sa.edu.kau.fcit.cpit252.project;
+
+public class Word {
+    public String word;
+    public String definition;
+}
