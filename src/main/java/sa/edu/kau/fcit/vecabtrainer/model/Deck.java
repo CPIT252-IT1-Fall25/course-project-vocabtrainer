c53d38ec73f903f106d0ce0018839e85241d55da
vAbdullh <136025903+vAbdullh@users.noreply.github.com>
2025-12-07 01:06:45 +0300

    create get endpoint to fetch decks

--- src/main/java/sa/edu/kau/fcit/vecabtrainer/model/Deck.java
+++ src/main/java/sa/edu/kau/fcit/vecabtrainer/model/Deck.java
@@ -1,4 +1,40 @@
 package sa.edu.kau.fcit.vecabtrainer.model;
 
 public class Deck {
+    private String deckId;
+    private String name;
+    private String description;
+    private String category;
+
+    public String getDeckId() {
+        return deckId;
+    }
+
+    public void setDeckId(String deckId) {
+        this.deckId = deckId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
